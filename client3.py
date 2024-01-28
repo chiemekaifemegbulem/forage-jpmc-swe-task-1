14e9d952c6431b24740a2bceb97bba055bc1664b
chiemekaifemegbulem <samuelifemegbulem@my.uopeople.edu>
2024-01-28 18:21:43 +0100

    updated getratio & getdatapoint methods

--- client3.py
+++ client3.py
@@ -30,19 +30,20 @@ N = 500
 
 
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
+    """Produce all the needed values to generate a datapoint"""
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Use the average of bid and ask as the price
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    """Get ratio of price_a and price_b"""
+    if price_b == 0:
+        # Avoid division by zero
+        return None
+    return price_a / price_b
 
 
 # Main
@@ -51,9 +52,23 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        # Initialize prices for ratio calculation
+        price_a = None
+        price_b = None
+
+        # Extract data points and calculate ratio
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+            # Update prices for ratio calculation
+            if price_a is None:
+                price_a = price
+            elif price_b is None:
+                price_b = price
+            else:
+                print("Ratio %s" % getRatio(price_a, price_b))
+                # Update prices for the next iteration
+                price_a = price
+                price_b = None
+
